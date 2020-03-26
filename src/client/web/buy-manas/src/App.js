dff93ec49cdd2485cfb0184c74b5f9d53f150b62
bekanur98 <bekanur98@gmail.com>
2020-03-27 00:30:27 +0600

    init new project with localize

--- src/client/web/buy-manas/src/App.js
+++ src/client/web/buy-manas/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useTranslation} from 'react-i18next';
 
 function App() {
+  const {t, i18n} = useTranslation();
+
+  function changeLang(lang = 'ky'){
+    i18n.changeLanguage(lang)
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
+
+        <button onClick={()=>changeLang('ky')}>KG</button>
+        <button onClick={()=>changeLang('ru')}>RU</button>
+        <button onClick={()=>changeLang('tr')}>TR</button>
+        <button onClick={()=>changeLang('en')}>EN</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +31,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>{t('hello')}</p>
       </header>
     </div>
   );
