5a9476867eb2d265383496ca8ffe3da7f27e675a
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-03-28 16:50:25 +0600

    mapping faculty names

--- src/client/web/buy-manas/src/components/Navbar/Categories/Categories.jsx
+++ src/client/web/buy-manas/src/components/Navbar/Categories/Categories.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import styles from './../Navbar.module.css'
 import { useTranslation } from 'react-i18next';
+import categoryIcon from './../../../assets/images/categoryIcon.png'
 
 
 const Categories = (props) => {
     const { t, i18n } = useTranslation();
 
-
     return (
         <div className={styles.categoriesBlock}>
             {
                 props.faculties.map(f => <div className={styles.categoriesItem} key={f.id}>
+                    <div className={styles.icon} ><img src={categoryIcon} alt="categoryIcon"/></div>
                     <p className={styles.facultyName}> {f.facultyNameKg} </p>
                 </div>)
             }
-            <div className={styles.categoriesItem}> <p>{t('postsTop')}</p> </div>
+            <div className={styles.categoriesItem}>
+                <div className={styles.icon} ><img src={categoryIcon} alt="categoryIcon"/></div>
+                 <p>{t('postsTop')}</p> </div>
         </div>
     )
 }
