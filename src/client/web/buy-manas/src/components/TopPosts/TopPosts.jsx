3e25408d745a8bf33fad9c1a5a02fd6089c1b757
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-03-28 16:51:16 +0600

    added topPosts component and container for this

---
+++ src/client/web/buy-manas/src/components/TopPosts/TopPosts.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './TopPosts.module.css'
+import { useTranslation } from 'react-i18next';
+import postImage from './../../assets/images/postImage.png'
+
+const TopPosts = (props) => {
+    const { t, i18 } = useTranslation();
+    return (
+        <div className={styles.topPostsWrapper}>
+            <h3>{t('postsTop')}</h3> 
+ 
+            <div className={styles.topPosts}>
+
+                {
+                    props.posts.map(tp => <div className={styles.topPost} key={tp.id}>
+                        <p className={styles.postTitle}> {tp.title} </p>
+                    </div>)
+                }
+
+            </div>
+        </div>
+    );
+}
+
+
+export default TopPosts;
+
+
