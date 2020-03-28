2cf87c97f4cfd62272dd306cc991a76829840517
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-03-28 15:16:34 +0600

    added maping of faculty names and posting

--- src/client/web/buy-manas/src/components/Navbar/Categories/Categories.jsx
+++ src/client/web/buy-manas/src/components/Navbar/Categories/Categories.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import styles from './../Navbar.module.css'
+import { useTranslation } from 'react-i18next';
 
 
 const Categories = (props) => {
+    const { t, i18n } = useTranslation();
 
-    console.log(props.state);
 
-    return(
-        <div className={styles.categoriesBlock}> 
-        {
-            props.faculties.map( f => <div key={f.id}>
-                <p className={styles.facultyName}>{f.facultyNameKg}</p>
-            </div> )
-        }
+    return (
+        <div className={styles.categoriesBlock}>
+            {
+                props.faculties.map(f => <div className={styles.categoriesItem} key={f.id}>
+                    <p className={styles.facultyName}> {f.facultyNameKg} </p>
+                </div>)
+            }
+            <div className={styles.categoriesItem}> <p>{t('postsTop')}</p> </div>
         </div>
     )
 }
