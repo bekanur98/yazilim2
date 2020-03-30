d2c2dbc620eaeb708072331bbef1669b8d5b9d9b
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-03-31 00:34:16 +0600

    created Footer component

---
+++ src/client/web/buy-manas/src/components/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './Footer.module.css'
+import { useTranslation } from 'react-i18next'; 
+import logo from './../../assets/images/logo.png'
+
+
+
+const Footer = (props) => {
+    const { t } = useTranslation();
+    return(
+        <div className={styles.footerWrapper}>
+            <div className={styles.rightSide}> 
+                <img className={styles.logo} src={logo} alt="logo" /> 
+                <p className={styles.copyright}>© 2020 {t('copyright')}</p>
+            </div>
+
+            <div className={styles.leftSide}>
+                <div className={styles.mediaLinks}>
+                    <a href="#">Facebook</a>
+                    <a href="#">Twitter</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
