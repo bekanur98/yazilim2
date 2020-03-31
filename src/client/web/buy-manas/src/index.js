bf36de8d7d8f0a76de6faa4be4eeb320ab73d5fc
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-03-31 19:18:36 +0600

    added BrowserRouter

--- src/client/web/buy-manas/src/index.js
+++ src/client/web/buy-manas/src/index.js
@@ -1,21 +1,24 @@
-import React, {Suspense} from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import rootReducer from './reducers'
 import './index.css';
 import App from './App';
 import './i18next';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
 const store = createStore(rootReducer)
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Suspense fallback={(<div>Loading ~~~~</div>)}>
-      <App/>
-    </Suspense>
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <Suspense fallback={(<div>Loading ~~~~</div>)}>
+        <App />
+      </Suspense>
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
