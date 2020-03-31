b2a76367ae9c2bd16db3a72b9cd387ae5d5d1076
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-03-31 19:21:49 +0600

    changed path

---
+++ src/client/web/buy-manas/src/components/Main/TopPosts/TopPosts.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styles from './TopPosts.module.css'
+import { useTranslation } from 'react-i18next';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const TopPosts = (props) => {
+    const { t } = useTranslation();
+    let settings = {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 2
+    };
+
+
+
+    return (
+        <div className={styles.topPostsWrapper}>
+            <h3>{t('postsTop')}</h3>
+
+            <div className={styles.topPosts} >
+                <Slider className={styles.slidersStyle} {...settings} >
+                    {
+                        props.posts.sort((a, b) => b.rating < a.rating)
+                            .map(tp => <div className={styles.topPostItem} key={tp.id}>
+                                <div className={styles.imgBlock}>
+                                    {tp.department ?
+                                        <img src={require('../../../assets/images/' + tp.department.faculty.id + '.png')} alt="categoryIcon" />
+                                        :
+                                        <img src={require('../../../assets/images/logo.png')} alt="categoryIcon" />
+                                    }
+                                </div>
+
+                                <p className={styles.cost}> {tp.cost != null ? tp.cost + ' сом' : t('contract')}</p>
+                                <p className={styles.postTitle}>{tp.title}</p>
+                                <p> Рейтинг {tp.rating} </p>
+                            </div>)
+                    }
+                </Slider>
+
+            </div>
+        </div>
+    );
+}
+
+
+export default TopPosts;
---
+++ src/client/web/buy-manas/src/components/Main/TopPosts/TopPostsContainer.jsx
@@ -0,0 +1,36 @@
+import React from 'react'; 
+import { connect } from 'react-redux';
+import * as axios from 'axios';
+import TopPosts from './TopPosts';
+import { setPostsAC } from './../../../actions/index';
+
+class TopPostsContainer extends React.Component{
+    componentDidMount() {
+        axios.get(`http://buymanasapi.ru.xsph.ru/index.php/api/posters.json`)
+            .then(response => {
+                this.props.setPosts(response.data);
+            });
+    }
+    render(){
+        return <TopPosts posts={ this.props.posts } />
+    }
+}
+
+const mapStateToProps = (state) => {
+    return{
+        posts: state.topPostsBlock.posts
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+        setPosts: (post) => {
+            dispatch(setPostsAC(post));
+        }
+    }
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(TopPostsContainer);
+
+
