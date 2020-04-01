5ffd8ad4fa2dfadb36ec121c335de9aab54ededa
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-02 01:58:12 +0600

    some refactoring

--- src/client/web/buy-manas/src/reducers/index.js
+++ src/client/web/buy-manas/src/reducers/index.js
@@ -13,5 +13,5 @@ export default combineReducers({
   categoriesBlock: categoriesReducer,
   topPostsBlock: topPostsReducer,
   auth: authReducer,
-  profileReducer
+  profilePage: profileReducer
 })
