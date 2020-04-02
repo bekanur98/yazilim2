28c01d8f5b252fa548d3e4b7d2a612f9872cbdee
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-02 21:56:02 +0600

    new component for ModalWindow

--- src/client/web/buy-manas/src/components/Modal/Modal.jsx
+++ src/client/web/buy-manas/src/components/Modal/Modal.jsx
@@ -2,20 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './Modal.module.css';
 
+const modalRoot = document.getElementById('modal-root');
 
 class Modal extends React.Component {
     componentWillMount(){
         this.root = document.createElement('div');
-        document.body.appendChild(this.root);
+        modalRoot.appendChild(this.root);
+        modalRoot.classList.add("modalWrapper")
     }
     componentWillUnmount(){
-        document.body.removeChild(this.root);
+        modalRoot.removeChild(this.root);
+        modalRoot.classList.remove("modalWrapper")
+
     }
 
     render(){
         return ReactDOM.createPortal(
             <div className={styles.modalWrapper}>
-                <button className={styles.btnClose} onClick={this.props.onClose}>x</button>
+                <div className={styles.btnCloseBlock} ><button className={styles.btnClose} onClick={this.props.onClose}>x</button></div>
+                <div className={styles.logoBlock}>
+                    <img src={require('../../assets/images/logo.png')} alt="LOGO"/>
+                </div>
                 {this.props.children}
             </div>,
             this.root
