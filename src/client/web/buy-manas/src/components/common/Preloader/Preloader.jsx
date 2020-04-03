60af3bf7009d657791af7422f4eb8af65cab7069
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-04 03:09:20 +0600

    added preloader(not currently in use)

---
+++ src/client/web/buy-manas/src/components/common/Preloader/Preloader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import preloader from '../../../assets/images/preloader.gif'
+
+const Prelodader = () => {
+    return (
+        <div>
+            {}
+            <img src={ preloader } alt="Loading"/>
+        </div>
+    )
+}
+
+export default Prelodader
