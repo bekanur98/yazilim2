11a263fc41a643cdab9b5d94e3509ce0ab976621
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-04 03:11:03 +0600

    some refactoring

--- src/client/web/buy-manas/src/components/Profile/Description/Description.jsx
+++ src/client/web/buy-manas/src/components/Profile/Description/Description.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './Description.module.css';
 import { useTranslation } from 'react-i18next';
-import Modal from '../../Modal/Modal';
+import Modal from '../../common/Modal/Modal';
 
 const Description = (props) => {
     const { t } = useTranslation();
-
     return (
         <div className={styles.descriptionWrapper}>
+            {/* {props.isFetching ? <Prelodader /> : null} */}
+
             <img className={styles.avatar} src={require('../../../assets/images/avatar.jpg')} alt="avatar" />
             <div className={styles.about}>
                 <div className={styles.names}>
@@ -15,29 +16,35 @@ const Description = (props) => {
                     <p className={styles.username}> @{props.username} </p>
                 </div>
                 <div className={styles.iDontKnow}>
-                    <div className={styles.faculty}> faculty </div>
-                    <div className={styles.postLength}> posters.length </div>
-                    <div className={styles.rating}> rating </div>
+                    <div className={styles.faculty}> <img src={require('../../../assets/images/' + props.faculty.id + '.png')} alt="Faculty" /> </div>
+                    <div className={styles.postLength}>
+                        <p> {props.posters.length} </p>
+                        <span> posts </span> 
+                    </div>
+                    <div className={styles.rating}>
+                        <p> 10 </p>
+                        <span> rating </span>
+                    </div>
                 </div>
             </div>
             <button className={styles.addPostBtn}> {t('Sell')} </button>
-            <button className={styles.editProfile} onClick={props.toggleModal}> {t('editProfile')} </button>
-            <div className={styles.phoneNumber}> props.phone </div>
+            <button className={styles.editProfile} onClick={props.toggleModalWindowEditProfile}> {t('editProfile')} </button>
+            <div className={styles.phoneNumber}> {props.phone} </div>
             {
                 props.isModalOpen &&
-                <Modal onClose={props.toggleModal}>
+                <Modal onClose={props.toggleModalWindowEditProfile}>
                     <form action="GET">
-                        
-                        <input type="text" placeholder={t('yourName')} />
-                        <input type="text" placeholder={t('yourEmail')} />
-                        <input type="text" placeholder={t('yourUsername')} />
+
+                        <input type="text" placeholder={t('yourName')} value={props.name} />
+                        <input type="text" placeholder={t('yourEmail')}  value={props.email} />
+                        <input type="text" placeholder={t('yourUsername')} value={props.username} />
                         <select id="faculties" name="faculties">
-                            <option value="volvo">Engineering</option> 
-                            <option value="volvo">Engineering</option> 
-                            <option value="volvo">Engineering</option> 
-                            <option value="volvo">Engineering</option> 
+                            <option value="volvo">Engineering</option>
+                            <option value="volvo">Engineering</option>
+                            <option value="volvo">Engineering</option>
+                            <option value="volvo">Engineering</option>
                         </select>
-                        <input type="phone" placeholder={t('yourNumber')} />
+                        <input type="phone" placeholder={t('yourNumber')} value={props.phone} />
                         <button> {t('saveChanges')} </button>
                     </form>
                 </Modal>
