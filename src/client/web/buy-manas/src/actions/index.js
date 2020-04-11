60b6decc2f3ed0027506a3484b6fbcbda5b4da2b
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-12 02:03:24 +0600

    added thunkCreators, renamed some actionCreators

--- src/client/web/buy-manas/src/actions/index.js
+++ src/client/web/buy-manas/src/actions/index.js
@@ -1,41 +1,39 @@
+import { postersApi, facultiesApi, usersApi, authApi} from "../api/api"
+
+
+// ACTIONS
+
 let nextTodoId = 0
 export const addTodo = text => ({
     type: 'ADD_TODO',
     id: nextTodoId++,
     text
 })
-
 export const setVisibilityFilter = filter => ({
     type: 'SET_VISIBILITY_FILTER',
     filter
 })
-
 export const toggleTodo = id => ({
     type: 'TOGGLE_TODO',
     id
 })
-
 export const VisibilityFilters = {
     SHOW_ALL: 'SHOW_ALL',
     SHOW_COMPLETED: 'SHOW_COMPLETED',
     SHOW_ACTIVE: 'SHOW_ACTIVE'
 }
 
-export const setFaculties = faculties => ({
+
+export const setFacultiesSuccess = faculties => ({
     type: 'SET_FACULTIES',
     faculties
 })
 
-export const setPosts = posts => ({
+export const setPostsSuccess = posts => ({
     type: 'SET_POSTS',
     posts
-})
+}) 
 
-export const setAuthUserData = (id, email, username) => ({ 
-    type: 'SET_AUTH_USER_DATA',
-    data: {id, email, username} 
-})
- 
 export const toggleModalWindowEditProfile = () => ({ 
     type: 'TOGGLE_MODAL_WINDOW_EDIT_PROFILE'
 })
@@ -47,7 +45,7 @@ export const toggleModalLoginAuth = () => ({
     type: 'TOGGLE_MODAL_LOGIN_AUTH'
 })
 
-export const setUserData = (userData) => ({ 
+export const setUserDataSuccess = (userData) => ({ 
     type: 'SET_USER_DATA',
     userData
 })
@@ -56,3 +54,58 @@ export const toggleIsFetching = (isFetching) => ({
     type: 'SET_IS_FETCHING', 
     isFetching  
 })
+
+export const setAuthUserData = (username, login, isAuth) => ({
+    type: 'SET_AUTH_USER_DATA',
+    payload: { username, login, isAuth }
+});
+
+// REDUX-THUNKS
+
+export const setPosts = () => (dispatch) => {
+    postersApi.getPosts()
+        .then(response => {
+            dispatch(setPostsSuccess(response.data));
+        });
+}
+
+export const setFaculties = () => (dispatch) => {
+    facultiesApi.getfaculties()
+        .then(response => {
+            dispatch(setFacultiesSuccess(response.data));
+        });
+}
+
+export const setUserData = () => (dispatch) => {
+    dispatch(toggleIsFetching(true));
+    usersApi.getUser(5)
+        .then(response => {
+            dispatch(setUserDataSuccess(response.data));
+            dispatch(toggleIsFetching(false));
+        });
+}
+
+ 
+
+
+export const authUserDataThunk = () => (dispatch) => {
+    authApi.me()
+        .then(response => {
+            if (response.data.resultCode === 0) {
+                let { username, password } = response.data
+                dispatch(setAuthUserData(username, password, true));
+            }
+        });
+}
+export const login = (userId, username, password, isAuth) => (dispatch) => {
+    authApi.login(userId, username, password, true)
+        .then(response => {
+            dispatch(authUserDataThunk())
+        });
+}
+export const logout = () => (dispatch) => {
+    authApi.logout()
+        .then(response => {
+            dispatch(setAuthUserData(null, null, null, false))
+        });
+}
