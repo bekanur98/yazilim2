e89dea10baa7264b0c267e2a89d058ed53bbb08e
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-13 18:16:03 +0600

    added if else for posts and sorted by date

--- src/client/web/buy-manas/src/components/Profile/MyPosts/MyPosts.jsx
+++ src/client/web/buy-manas/src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,22 +6,34 @@ import { IMAGES_URL } from '../../../constants';
 
 const MyPosts = (props) => {
     const { t } = useTranslation();
+
+    const LastPostsArray = [];
+    props.posts.sort((a, b) => {
+        return new Date(b.publishedAt) - new Date(a.publishedAt);
+    }).map(p => {
+        LastPostsArray.push(p);
+    });
     return (
         <div className={styles.myPostsWrapper}>
             <h3>Мои посты</h3>
             <div className={styles.myPosts}>
-                { 
-                    props.posts.map(tp => <div className={styles.myPostItem} key={tp.id}>
-                        <div className={styles.imgBlock}> 
-                            {tp.images.length ?  
-                                <img src={IMAGES_URL + tp.images[0].url} alt="categoryIcon" />
-                                :
-                                <img src={require('../../../assets/images/logo.png')} alt="categoryIcon" />
-                            }
-                        </div>
-                        <p className={styles.cost}>Цена: {tp.cost != null ? tp.cost + ' сом' : t('contract')}</p>
-                        <p className={styles.postTitle}>{tp.title}</p>
-                    </div>)
+
+            {
+                    LastPostsArray.length 
+                        ? LastPostsArray.map(p => <div className={styles.myPostItem} key={p.id}>
+                            <div className={styles.imgBlock}>
+                                {p.images.length ?
+                                    <img src={IMAGES_URL + p.images[0].url} alt="categoryIcon" />
+                                    :
+                                    <img src={require('../../../assets/images/logo.png')} alt="categoryIcon" />
+                                }
+                            </div>
+
+                            <p className={styles.cost}> {p.cost != null ? p.cost + ' сом' : t('contract')}</p>
+                            <p className={styles.postTitle}>{p.title}</p>
+                        </div>)
+                        : <p className={styles.nullPosts}>Пока нет постов((</p>
+                         
                 }
             </div>
         </div>
