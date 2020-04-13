0354f12b36f9514d6ec078ff26fc97ace5078005
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-13 18:24:33 +0600

    created component for showing posts of every faculty

---
+++ src/client/web/buy-manas/src/components/FacultiesPosts/FacultiesPosts.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styles from './FacultiesPosts.module.css'
+import { useTranslation } from 'react-i18next';
+import { IMAGES_URL } from '../../constants';
+
+const FacultiesPosts = (props) => {
+    const { t } = useTranslation();
+    const LastPostsArray = [];
+    props.posts.filter(p => p.department != null && p.department.faculty.id == props.match.params.facultyId)
+        .sort((a, b) => {
+            return new Date(b.publishedAt) - new Date(a.publishedAt);
+        }).map(p => {
+            LastPostsArray.push(p);
+        });
+    return (
+        <div className={styles.facultyPostsWrapper}>
+            <h3>ПОСТЫЫ</h3>
+
+            <div className={styles.facultyPosts} >
+                {
+                    LastPostsArray.length 
+                        ? LastPostsArray.map(p => <div className={styles.facultyPostItem} key={p.id}>
+                            <div className={styles.imgBlock}>
+                                {p.images.length ?
+                                    <img src={IMAGES_URL + p.images[0].url} alt="categoryIcon" />
+                                    :
+                                    <img src={require('../../assets/images/logo.png')} alt="categoryIcon" />
+                                }
+                            </div>
+                            <div className={styles.descriptionBlock}>
+                                <p className={styles.postTitle}>{p.title}</p>
+                                <p className={styles.description}>{p.description}</p>
+                                <p className={styles.cost}> {p.cost != null ? p.cost + ' сом' : t('contract')}</p>
+
+                            </div>
+
+                        </div>)
+                        : <p className={styles.nullPosts}>Пока нет постов((</p>
+                         
+                }
+            </div>
+        </div>
+    )
+}
+
+export default FacultiesPosts
