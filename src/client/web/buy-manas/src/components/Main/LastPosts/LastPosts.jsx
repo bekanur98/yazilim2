7f32ad6b7f759fb2b789f4e274fdf3df9254a133
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-13 18:20:44 +0600

    removed Slider, settings for that, removed restriction for last posts

--- src/client/web/buy-manas/src/components/Main/LastPosts/LastPosts.jsx
+++ src/client/web/buy-manas/src/components/Main/LastPosts/LastPosts.jsx
@@ -3,27 +3,18 @@ import styles from './LastPosts.module.css'
 import { useTranslation } from 'react-i18next';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
 import { IMAGES_URL } from '../../../constants';
 
 const LastPosts = (props) => {
 
-    const { t } = useTranslation();
-    let settings = {
-        dots: false,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 5,
-        slidesToScroll: 1
-    };
+    const { t } = useTranslation(); 
 
 
     // cicle for top 10 posts 
     const LastPostsArray = [];
     props.posts.sort((a, b) => {
         return new Date(b.publishedAt) - new Date(a.publishedAt);
-    }).map(p=>{
-        if(LastPostsArray.length<=9)
+    }).map(p => {
         LastPostsArray.push(p);
     });
 
@@ -34,8 +25,7 @@ const LastPosts = (props) => {
         <div className={styles.LastPostsWrapper}>
             <h3>{t('postsLast')}</h3>
 
-            <div className={styles.lastPosts} >
-                <Slider className={styles.slidersStyle} {...settings} >
+            <div className={styles.lastPosts} > 
                     {
                         LastPostsArray.map(p => <div className={styles.lastPostItem} key={p.id}>
                             <div className={styles.imgBlock}>
@@ -49,10 +39,7 @@ const LastPosts = (props) => {
                             <p className={styles.cost}> {p.cost != null ? p.cost + ' сом' : t('contract')}</p>
                             <p className={styles.postTitle}>{p.title}</p>
                         </div>)
-                    }
-
-                </Slider>
-
+                    } 
             </div>
         </div>
     );
