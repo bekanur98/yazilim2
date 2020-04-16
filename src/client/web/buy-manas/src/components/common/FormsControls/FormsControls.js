af595738a8e4b209301fad32b092c196da698271
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-16 19:11:34 +0600

    default form inputs with showing error

---
+++ src/client/web/buy-manas/src/components/common/FormsControls/FormsControls.js
@@ -0,0 +1,26 @@
+import React from 'react'; 
+import styles from './FormsControls.module.css'; 
+import { useTranslation } from 'react-i18next';
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
+
+export const Input = ({input, meta, ...props}) => {
+    const hasError = meta.touched && meta.error
+    return(
+        <div className={styles.formControl + ' ' + (hasError ? styles.error : '') }>
+            <div> <input type="text" {...input} {...props} autoComplete="off"/> </div>
+            { hasError && <span>{meta.error}</span> } 
+        </div>
+    )
+}
+
+export const Phone = ({input, meta, ...props}) => { 
+    const {t} = useTranslation();
+    return(
+        <div className={styles.formControl}>
+            <div> 
+                <PhoneInput {...input} {...props} country={'kg'} onlyCountries={['kg', 'kz', 'ru']} placeholder={t('yourNumber')} /> 
+            </div> 
+        </div>
+    )
+}
