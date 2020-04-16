7024415188dffd6c72c6d5699823faaa1d9de443
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-16 18:59:38 +0600

    added Route for page of posts and page of new post

--- src/client/web/buy-manas/src/App.js
+++ src/client/web/buy-manas/src/App.js
@@ -6,15 +6,19 @@ import Main from './components/Main/Main';
 import Profile from './components/Profile/Profile';
 import { Route } from 'react-router-dom';
 import FacultiesPostsContainer from './components/FacultiesPosts/FacultiesPostsContainer';
+import PostsPageContainer from './components/Main/PostsPage/PostsPageContainer';
+import NewPostContainer from './components/NewPost/NewPostContainer';
 
 const App = (props) => {
 
     return (
         <div className="appWrapper">
             <HeaderContainer />
-            <Route exact path='/main' render={() => <Main />} />
-            <Route exact path='/profile' render={() => <Profile />} />            
-            <Route path='/facultiesPosts/:facultyId' render={() => <FacultiesPostsContainer />} />            
+            <Route path='/main' render={() => <Main />} />
+            <Route path='/profile' render={() => <Profile />} />            
+            <Route exact path='/facultiesPosts/:facultyId' render={() => <FacultiesPostsContainer />} />            
+            <Route path={'/posts/:postsId'} render={() => <PostsPageContainer />} />            
+            <Route path={'/newPost'} render={() => <NewPostContainer />} />            
             <Footer />
         </div>
     );
