71fa52b85e580acca969d26fea1fd20eac97ce73
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-16 19:12:37 +0600

    added spaces and semicolons

--- src/client/web/buy-manas/src/components/common/Modal/Modal.jsx
+++ src/client/web/buy-manas/src/components/common/Modal/Modal.jsx
@@ -5,24 +5,23 @@ import styles from './Modal.module.css';
 const modalRoot = document.getElementById('modal-root');
 
 class Modal extends React.Component {
-    componentWillMount(){
+    componentWillMount() {
         this.root = document.createElement('div');
         modalRoot.appendChild(this.root);
-        modalRoot.classList.add("modalWrapper")
+        modalRoot.classList.add("modalWrapper");
     }
-    componentWillUnmount(){
+    componentWillUnmount() {
         modalRoot.removeChild(this.root);
-        modalRoot.classList.remove("modalWrapper")
-
+        modalRoot.classList.remove("modalWrapper");
     }
 
-    render(){
+    render() {
         return ReactDOM.createPortal(
             <div className={styles.modalWrapper}>
                 <div className={styles.btnCloseBlock} ><button className={styles.btnClose} onClick={this.props.onClose}>x</button></div>
                 <div className={styles.logoBlock}>
-                    <img src={require('../../../assets/images/logo.png')} alt="LOGO"/>
-                </div>
+                    <img src={require('../../../assets/images/logo.png')} alt="LOGO" />
+                </div> 
                 <div className={styles.modalBody}>
                     {this.props.children}
                 </div>
