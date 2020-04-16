34724993ea9a6d519ea55900fda2147edb1bdddc
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-16 19:05:27 +0600

    created component for RegisterForm from Header

---
+++ src/client/web/buy-manas/src/components/Header/RegisterForm/RegisterForm.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { getLocale } from '../../../i18next';
+import { Input, Phone } from '../../common/FormsControls/FormsControls'
+import { maxLengthCreator, minLengthCreator, required, emailValid, matchInput } from '../../../utils/validators/validators'
+import { useTranslation } from 'react-i18next';
+import styles from '../Header.module.css';
+
+const RegisterForm = (props) => {
+    const { t } = useTranslation();
+    const facultyName = 'facultyName' + getLocale().charAt(0).toUpperCase() + getLocale().slice(1);
+    const maxLength30 = maxLengthCreator(30);
+    const maxLength13 = maxLengthCreator(13);
+    const minLength8 = minLengthCreator(8); 
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <Field component={Input} name='name' type="text" placeholder={t('yourName')} validate={[required]} />
+            <Field component={Input} name='username' type="text" placeholder={t('yourUsername')} validate={[required, maxLength30]} />
+            <Field component={Input} name='email' type="text" placeholder={t('yourEmail')} validate={[required, emailValid]} />
+            <Field component={Phone} name='number' validate={[required, maxLength13]} />
+            <Field component="select" name="faculty">
+                {props.faculties.map(f => <option key={f.id}>{f[facultyName]}</option>)}
+            </Field>
+            <Field component={Input} name='regPassword' type="password" placeholder={t('password')} validate={[required, minLength8]} />
+            <Field component={Input} name='confirmPassword' type="password" placeholder={t('password')} validate={[required, matchInput]} />
+            { props.error && <div className={styles.wrongData}>{ props.error }</div> }
+            <button> {t('signUp')} </button>
+            <p className={styles.signUp}>
+                Есть аккаунт?
+                <span className={styles.clickableLink} onClick={props.toggleModalLoginAuth}>
+                    Вход
+                </span>
+            </p>
+        </form>
+    )
+}
+
+export default reduxForm({ form: 'register' })(RegisterForm);
