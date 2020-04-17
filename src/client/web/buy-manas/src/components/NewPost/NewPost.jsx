e438eb78ffe3a955ee9848f5eaae47bacf0ffbe3
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-18 01:49:08 +0600

    added function to send datas of new post

--- src/client/web/buy-manas/src/components/NewPost/NewPost.jsx
+++ src/client/web/buy-manas/src/components/NewPost/NewPost.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import styles from './NewPost.module.css';
 import { Field, reduxForm } from 'redux-form';
-import { Input } from '../common/FormsControls/FormsControls';
+import { Input, Textarea } from '../common/FormsControls/FormsControls';
 import { required } from '../../utils/validators/validators';
 
 const NewPost = (props) => {
+    let date = new Date();
+    let submit = (value) => {
+        debugger
+        // let obj = {
+        //     ...value,
+        //     publishedAt: date,
+        //     author: {
+        //         id: props.id,
+        //         name: props.name,
+        //         images: []
+        //     },
+        //     department: {
+        //         faculty:{
+        //             id: 5
+        //         }
+        //     }
+        // }
+
+        props.newPost(value)
+    };
+
     return(
         <div className={styles.newPostWrapper}>
-            <form onSubmit={props.handleSubmti}>
+            <form onSubmit={props.handleSubmit(submit)}>
                 <div className={styles.newImage}>
                     <p className={styles.blockName}>Загрузите фотографии</p>                
                     <div className={styles.inputs}></div>
@@ -23,14 +44,14 @@ const NewPost = (props) => {
                 <div className={styles.newDescr}>
                     <p className={styles.blockName}>Описание</p>                
                     <div className={styles.inputs}>
-                        <Field component={Input} name='description' type='text' placeholder='Описание' validate={[required]} />
+                        <Field component={Textarea} name='description' type='text' placeholder='Описание' validate={[required]} />
                     </div>
                 </div>
 
                 <div className={styles.newCost}>
                     <p className={styles.blockName}>Цена</p>                
                     <div className={styles.inputs}>
-                        <Field component={Input} name='cost' type='text' placeholder='Цена' validate={[required]} />
+                        <Field component={Input} name='cost' type='number' placeholder='Цена' validate={[required]} />
                     </div>
                 </div> 
 
