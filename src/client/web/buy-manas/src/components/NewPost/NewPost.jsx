d92a5f1bd67c832bda4f1dccb4932b87e005ff58
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-20 02:30:42 +0600

    added select for choose department, now inputs will empty after posting

--- src/client/web/buy-manas/src/components/NewPost/NewPost.jsx
+++ src/client/web/buy-manas/src/components/NewPost/NewPost.jsx
@@ -3,26 +3,33 @@ import styles from './NewPost.module.css';
 import { Field, reduxForm } from 'redux-form';
 import { Input, Textarea } from '../common/FormsControls/FormsControls';
 import { required } from '../../utils/validators/validators';
+import { compose } from 'redux';
+import { withAuthRedirect } from '../../hoc/withAuthRedirect';
+import { getLocale } from '../../i18next';
 
 const NewPost = (props) => {
+    const facultyName = 'facultyName' + getLocale().charAt(0).toUpperCase() + getLocale().slice(1);
+    const depName = 'dep_name_' + getLocale().charAt(0).toLowerCase() + getLocale().slice(1);
     let date = new Date(); 
     const uploadImage = e => {
         if(e.target.files.length){
             props.newCurrentImage(e.target.files[0]);
-        } else {
-            return []
-        }
+        }  
     } 
     let submit = (value) => {
+        debugger
         let obj = {
             ...value,
             publishedAt: date,
-            author: `/api/users/${props.userId}`,
+            author: `/api/users/${props.userId}`, 
             rating: 0,
             images: props.image
         }
+        props.newPostImage(obj);
+        value.title = '';
+        value.description = '';
+        value.cost = ''; 
 
-        props.newPostImage(obj)
     };
 
     return(
@@ -34,39 +41,51 @@ const NewPost = (props) => {
                 </div>
 
                 <div className={styles.newTitle}>
-                    <p className={styles.blockName}>Название</p>                
+                    <p className={styles.blockName}>Название <sup>*</sup> </p>                
                     <div className={styles.inputs}>
                         <Field component={Input} name='title' type='text' placeholder='Название' validate={[required]} />
                     </div>
                 </div>
 
                 <div className={styles.newDescr}>
-                    <p className={styles.blockName}>Описание</p>                
+                    <p className={styles.blockName}>Описание <sup>*</sup></p>                
                     <div className={styles.inputs}>
                         <Field component={Textarea} name='description' type='text' placeholder='Описание' validate={[required]} />
                     </div>
                 </div>
-{/* 
-                <div className={styles.newDep}>
-                    <p className={styles.blockName}>Категория</p>                
+
+                {/* <div className={styles.newFaculty}>
+                    <p className={styles.blockName}>Выберите факультет</p>                
                     <div className={styles.inputs}>
-                        <Field component={Input} name='department' type='text' placeholder='Описание' validate={[required]} />
+                        <Field component='select' name='faculties' >
+                            {props.faculties.map(f => <option key={f.id} value={f.id}>{f[facultyName]}</option>)} 
+                        </Field>
                     </div>
                 </div> */}
 
+                <div className={styles.newDep}>
+                    <p className={styles.blockName}>Выберите департамент</p>                
+                    <div className={styles.inputs}>
+                        <Field component='select' name='department' >
+                            <option></option>
+                            {props.departments.map(d => <option key={d.id} value={d.id}>{d[depName]}</option>)} 
+                        </Field>
+                    </div>
+                </div>
+
                 <div className={styles.newCost}>
-                    <p className={styles.blockName}>Цена</p>                
+                    <p className={styles.blockName}>Цена <sup>*</sup></p>                
                     <div className={styles.inputs}>
                         <Field component={Input} name='cost' type='number' placeholder='Цена' validate={[required]} />
                     </div>
                 </div> 
-
+                { props.error && <div className={styles.successSubmit}>{ props.error }</div> }
                 <div className={styles.submit}>
                     <button>Опубликовать</button>
                 </div>
             </form>
-        </div>
+        </div>  
     )
 }
 
-export default reduxForm({form: 'newPost'})(NewPost);
+export default compose( withAuthRedirect, reduxForm({form: 'newPost'}) ) (NewPost);
