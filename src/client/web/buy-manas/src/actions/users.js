c3558d8a96ea91e007b5cd369c59e69bd5ef5a83
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-23 03:24:32 +0600

    added thunkCreator for editProfile and newAvatar

--- src/client/web/buy-manas/src/actions/users.js
+++ src/client/web/buy-manas/src/actions/users.js
@@ -1,4 +1,5 @@
 import { usersApi } from "../api/api"
+import { stopSubmit } from 'redux-form'
 
 
 // ACTION CREATORS
@@ -14,6 +15,22 @@ export const toggleIsFetching = (isFetching) => ({
     isFetching
 })
 
+export const editProfileSuccess = (profileData) => ({
+    type: 'EDIT_PROFILE',
+    profileData
+})
+
+
+export const newAvatar = (avatar) => ({
+    type: 'NEW_AVATAR',
+    avatar
+})
+
+
+export const toggleModalWindowEditProfile = () => ({
+    type: 'TOGGLE_MODAL_WINDOW_EDIT_PROFILE'
+})
+
 // REDUX-THUNKS
 
 export const setUserData = (userId) => (dispatch) => {
@@ -25,3 +42,39 @@ export const setUserData = (userId) => (dispatch) => {
         });
 }
 
+
+export const editProfile = (userId, profileData) => (dispatch) => {
+    debugger
+    if (profileData.avatar) {
+        debugger
+        usersApi.newAvatar(userId, profileData.avatar)
+            .then(r => {
+                debugger
+                if (r.data.url) {
+                    debugger
+                    usersApi.editProfile(userId, profileData, r)
+                        .then(r => {
+                            debugger
+                            if (r.status === 200) {
+                                dispatch(editProfileSuccess(r.data));
+                                dispatch(stopSubmit('editProfile', { _error: 'Изменения сохранены' }));
+                                dispatch(toggleModalWindowEditProfile(false))
+                                debugger
+                            }
+                        })
+                }
+            })
+    } else {
+        debugger
+        usersApi.editProfile(userId, profileData)
+            .then(r => {
+                debugger
+                if (r.status === 200) {
+                    dispatch(editProfileSuccess(r.data));
+                    dispatch(stopSubmit('editProfile', { _error: 'Изменения сохранены' }));
+                    dispatch(toggleModalWindowEditProfile(false));
+                    debugger
+                }
+            })
+    }
+}
