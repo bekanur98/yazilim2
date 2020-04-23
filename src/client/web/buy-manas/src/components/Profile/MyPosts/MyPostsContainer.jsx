28ba39f32dd17c1f8522b0c4356b23152c418a55
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-24 05:22:38 +0600

    added state of posts from profileData

--- src/client/web/buy-manas/src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/client/web/buy-manas/src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import MyPosts from './MyPosts';
 import { setUserData, toggleIsFetching } from '../../../actions/users';
+import { setPosts } from '../../../actions/posts';
 
 
 class MyPostsContainer extends React.Component{
     componentDidMount() {
         this.props.setUserData(this.props.id);
+        this.props.setPosts(this.props.id);
     }
     render(){
         return <MyPosts {...this.props} />
@@ -15,13 +17,13 @@ class MyPostsContainer extends React.Component{
 
 const mapStateToProps = (state) => {
     return{
-        posts: state.auth.posters,
+        posts: state.profilePage.posters,
         id: state.auth.id,
         isFetching: state.profilePage.isFetching
     }
 }; 
 
 
-export default connect(mapStateToProps, {setUserData, toggleIsFetching})(MyPostsContainer);
+export default connect(mapStateToProps, {setUserData, toggleIsFetching, setPosts})(MyPostsContainer);
 
 
