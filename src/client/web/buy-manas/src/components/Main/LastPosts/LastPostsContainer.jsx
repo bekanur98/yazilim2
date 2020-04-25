48760c385e70321b7aee641f81bfaceb56742110
bekanur98 <bekanur98@gmail.com>
2020-04-25 17:22:45 +0600

    added: detect scroll page on bottom

--- src/client/web/buy-manas/src/components/Main/LastPosts/LastPostsContainer.jsx
+++ src/client/web/buy-manas/src/components/Main/LastPosts/LastPostsContainer.jsx
@@ -1,15 +1,36 @@
-import React from 'react'; 
+import React from 'react';
 import { connect } from 'react-redux';
 import LastPosts from './LastPosts';
 import { setPosts, setPostByTitle, setCurrentPage } from '../../../actions/posts';
 import { toggleIsFetching } from '../../../actions/users';
 
-class LastPostsContainer extends React.Component { 
+class LastPostsContainer extends React.Component {
+
+    isBottom(el) {
+        return el.getBoundingClientRect().bottom <= window.innerHeight;
+    }
 
     componentDidMount() {
-        this.props.setPosts(); 
-    }  
- 
+        this.props.setPosts();
+        document.addEventListener('scroll', this.trackScrolling);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('scroll', this.trackScrolling);
+    }
+
+    trackScrolling = () => {
+        const wrappedElement = document.getElementById('root');
+        if (this.isBottom(wrappedElement)) {
+            console.log('header bottom reached');
+            document.removeEventListener('scroll', this.trackScrolling);
+        }
+    };
+
+    // componentDidMount() {
+    //     this.props.setPosts();
+    // }
+
     render(){
         return <LastPosts {...this.props} loadMore={this.loadMore} state={this.state} nextPage={this.nextPage} />
     }
@@ -21,7 +42,7 @@ const mapStateToProps = (state) => {
         searchedPosts: state.postsData.searchedPosts,
         currentPage: state.postsData.currentPage
     }
-}; 
+};
 
 export default connect(mapStateToProps, {setPosts, setPostByTitle, toggleIsFetching, setCurrentPage})(LastPostsContainer);
 
