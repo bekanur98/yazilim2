b21d1e7f3ec987e9959f8ead1733968537b90e19
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-25 16:46:21 +0600

    added functions for future scroll pagination

--- src/client/web/buy-manas/src/components/Main/LastPosts/LastPostsContainer.jsx
+++ src/client/web/buy-manas/src/components/Main/LastPosts/LastPostsContainer.jsx
@@ -1,25 +1,28 @@
 import React from 'react'; 
 import { connect } from 'react-redux';
 import LastPosts from './LastPosts';
-import { setPosts, setPostByTitle } from '../../../actions/posts';
+import { setPosts, setPostByTitle, setCurrentPage } from '../../../actions/posts';
 import { toggleIsFetching } from '../../../actions/users';
 
-class LastPostsContainer extends React.Component{ 
-    componentDidMount() { 
-        this.props.setPosts();  
-    } 
+class LastPostsContainer extends React.Component { 
+
+    componentDidMount() {
+        this.props.setPosts(); 
+    }  
+ 
     render(){
-        return <LastPosts {...this.props} loadMore={this.loadMore} state={this.state}/>
+        return <LastPosts {...this.props} loadMore={this.loadMore} state={this.state} nextPage={this.nextPage} />
     }
 }
 
 const mapStateToProps = (state) => {
     return{
         posts: state.postsData.posts,
-        searchedPosts: state.postsData.searchedPosts
+        searchedPosts: state.postsData.searchedPosts,
+        currentPage: state.postsData.currentPage
     }
 }; 
 
-export default connect(mapStateToProps, {setPosts, setPostByTitle, toggleIsFetching})(LastPostsContainer);
+export default connect(mapStateToProps, {setPosts, setPostByTitle, toggleIsFetching, setCurrentPage})(LastPostsContainer);
 
 
