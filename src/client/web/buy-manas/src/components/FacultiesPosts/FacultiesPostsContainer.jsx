35cd8fa53731ff731d8d7ab39097cfc36bc09c21
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-27 07:28:23 +0600

    added funciton for tracking scroll and posting next page of posts

--- src/client/web/buy-manas/src/components/FacultiesPosts/FacultiesPostsContainer.jsx
+++ src/client/web/buy-manas/src/components/FacultiesPosts/FacultiesPostsContainer.jsx
@@ -4,12 +4,36 @@ import { withRouter } from 'react-router-dom';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { getFacultiesPosts } from '../../actions/faculties';
+import { setCurrentPage } from '../../actions/posts';
+
+
 
 
 class FacultiesPostsContainer extends React.Component{
+    
+    isBottom(el) {
+        return el.getBoundingClientRect().bottom <= window.innerHeight;
+    } 
+
+    componentWillUnmount() {
+        document.removeEventListener('scroll', this.trackScrolling);
+    }
+
+    trackScrolling = () => {
+        const wrappedElement = document.getElementById('root');
+        const facultyId = this.props.match.params.facultyId;
+
+        if (this.isBottom(wrappedElement)) {
+            this.props.setCurrentPage(this.props.currentPage + 1);
+            this.props.getFacultiesPosts(facultyId, this.props.currentPage);
+            document.removeEventListener('scroll', this.trackScrolling); 
+        }
+    }; 
+
     componentDidMount(){
-        let facultyId = this.props.match.params.facultyId
+        const facultyId = this.props.match.params.facultyId;
         this.props.getFacultiesPosts(facultyId)
+        document.addEventListener('scroll', this.trackScrolling)
     }
 
     render(){
@@ -19,11 +43,12 @@ class FacultiesPostsContainer extends React.Component{
 
 const mapStateToProps = (state) => {
     return{
-        posts: state.postsData.postsOfFaculty
+        posts: state.postsData.postsOfFaculty,
+        currentPage: state.postsData.currentPage
     }        
 }
 
 export default compose(
-    connect(mapStateToProps, { getFacultiesPosts }),
+    connect(mapStateToProps, { getFacultiesPosts, setCurrentPage }),
     withRouter
 )(FacultiesPostsContainer);  
