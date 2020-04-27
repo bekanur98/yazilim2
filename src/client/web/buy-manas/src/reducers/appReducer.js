63d48c3cb03e8f7f6827d7f06d370858979ed7cc
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-27 07:23:06 +0600

    created appReducer for initialize logging user

---
+++ src/client/web/buy-manas/src/reducers/appReducer.js
@@ -0,0 +1,21 @@
+
+let initialState = { 
+    initialized: false
+};
+const appReducer = (state = initialState, action) => {
+
+    switch(action.type){ 
+        
+        case 'INITIALIZED_SUCCESS':{
+            return{
+                ...state,
+                initialized: true
+            }
+        }
+        default:
+            return state;
+    } 
+
+}; 
+
+export default appReducer;
