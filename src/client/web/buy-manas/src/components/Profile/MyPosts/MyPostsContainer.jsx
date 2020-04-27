0cd97c99560e3aa6420296dffacfeeb520cb4a3e
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-27 07:23:36 +0600

    added cookies and function for tracking scrolling and posting next page of posts

--- src/client/web/buy-manas/src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/client/web/buy-manas/src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,32 +1,51 @@
-import React from 'react'; 
+import React from 'react';
 import { connect } from 'react-redux';
 import MyPosts from './MyPosts';
 import { setUserData, toggleIsFetching } from '../../../actions/users';
-import { setPosts } from '../../../actions/posts';
+import { setPosts, setCurrentPage } from '../../../actions/posts';
+import Cookies from 'universal-cookie';
 
 
-class MyPostsContainer extends React.Component{
-    componentDidMount() {
-        if(this.props.id){
-            this.props.setUserData(this.props.id);
-        }
-        this.props.setPosts();
+class MyPostsContainer extends React.Component {
+    isBottom(el) {
+        return el.getBoundingClientRect().bottom <= window.innerHeight;
     }
-    render(){
+
+    componentDidMount() {
+        let cookies = new Cookies();  
+        document.addEventListener('scroll', this.trackScrolling)
+        this.props.setUserData(cookies.get('id'));
+
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('scroll', this.trackScrolling);
+    }
+
+    trackScrolling = () => {
+        const wrappedElement = document.getElementById('root');
+        if (this.isBottom(wrappedElement)) {
+            this.props.setCurrentPage(this.props.currentPage + 1);
+            this.props.setPosts(this.props.currentPage);
+            document.removeEventListener('scroll', this.trackScrolling);
+        }
+    };
+
+    render() {
         return <MyPosts {...this.props} />
     }
 }
 
 const mapStateToProps = (state) => {
-    return{
+    return {
         posts: state.profilePage.posters,
-        posts2: state.auth.posters,
         id: state.auth.id,
-        isFetching: state.profilePage.isFetching
+        isFetching: state.profilePage.isFetching,
+        currentPage: state.postsData.currentPage
     }
-}; 
+};
 
 
-export default connect(mapStateToProps, {setUserData, toggleIsFetching, setPosts})(MyPostsContainer);
+export default connect(mapStateToProps, { setUserData, toggleIsFetching, setPosts, setCurrentPage })(MyPostsContainer);
 
 
