e98221398809d1154321c1321c01268f9203cd0c
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-27 07:23:55 +0600

    removed useless code

--- src/client/web/buy-manas/src/components/Profile/MyPosts/MyPosts.jsx
+++ src/client/web/buy-manas/src/components/Profile/MyPosts/MyPosts.jsx
@@ -8,25 +8,18 @@ import Preloader from '../../common/Preloader/Preloader';
 const MyPosts = (props) => { 
     const LastPostsArray = [];
 
-    if (props.posts2) {
-        props.posts2.sort((a, b) => {
-            return new Date(b.publishedAt) - new Date(a.publishedAt);
-        }).map(p => {
-            LastPostsArray.push(p);
-        });
-    }
+    props.posts.sort((a, b) => {
+        return new Date(b.publishedAt) - new Date(a.publishedAt);
+    }).map(p => {
+        LastPostsArray.push(p);
+    });
+    
     return (
         <div className={styles.myPostsWrapper}>
             <h3>Мои посты</h3>
             <div className={styles.myPosts}>
 
-                {props.post
-                    && props.posts.sort((a, b) => {
-                        return new Date(b.publishedAt) - new Date(a.publishedAt);
-                    }).map(p => {
-                        LastPostsArray.push(p);
-                    })
-                }
+                {props.isFetching ? <Preloader /> : null}
 
                 {LastPostsArray.length
                     ? LastPostsArray.map(l => <NavLink to={`posts/${l.id}`}>
