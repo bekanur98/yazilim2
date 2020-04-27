6fef1e65cfd97da17417c917af31f9e0958fc5c6
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-04-27 07:26:30 +0600

    changed to class componetnt, added compose,withRouter,connect

--- src/client/web/buy-manas/src/App.js
+++ src/client/web/buy-manas/src/App.js
@@ -4,25 +4,44 @@ import HeaderContainer from './components/Header/HeaderContainer'
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
 import Profile from './components/Profile/Profile';
-import { Route } from 'react-router-dom';
+import { Route, withRouter } from 'react-router-dom';
 import FacultiesPostsContainer from './components/FacultiesPosts/FacultiesPostsContainer';
 import PostsPageContainer from './components/Main/PostsPage/PostsPageContainer';
 import NewPostContainer from './components/NewPost/NewPostContainer';
+import { connect } from 'react-redux';
+import { initializeApp } from './actions/login'
+import { setPosts } from './actions/posts'
+import { compose } from 'redux';
 
-const App = (props) => {
+class App extends React.Component {
 
-    return (
-        <div className="appWrapper">
-            <HeaderContainer />
-            <Route exact path='/' render={() => <Main />} />
-            <Route path='/profile' render={() => <Profile />} />            
-            <Route path='/facultiesPosts/:facultyId' render={() => <FacultiesPostsContainer />} />
-            <Route path={'/facultiesPosts/:facultyId/posts/:postsId'} render={() => <PostsPageContainer />} />                        
-            <Route path={'/posts/:postsId'} render={() => <PostsPageContainer />} />            
-            <Route path={'/newPost'} render={() => <NewPostContainer />} />            
-            <Footer />
-        </div>
-    );
+    componentDidMount(){
+        this.props.setPosts();
+        this.props.initializeApp();
+   }
+
+    render() {
+        return (
+            <div className="appWrapper">
+                <HeaderContainer />
+                <Route exact path='/' render={() => <Main />} />
+                <Route path='/profile' render={() => <Profile />} />
+                <Route path='/facultiesPosts/:facultyId' render={() => <FacultiesPostsContainer />} />
+                <Route path={'/facultiesPosts/:facultyId/posts/:postsId'} render={() => <PostsPageContainer />} />
+                <Route path={'/posts/:postsId'} render={() => <PostsPageContainer />} />
+                <Route path={'/newPost'} render={() => <NewPostContainer />} />
+                <Footer />
+            </div>
+        );
+    }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+    initialized: state.app.initialized
+})
+
+export default compose(
+    withRouter,
+    connect(mapStateToProps, {initializeApp, setPosts})
+)
+(App);
