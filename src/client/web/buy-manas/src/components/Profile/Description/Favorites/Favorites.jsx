4bf968d9967ffc82109fcec221fcaf3aa622dd06
Mamytov Kushbak <mamytovkushbak@gmail.com>
2020-05-01 00:04:54 +0600

    created component for showing favorite posts

---
+++ src/client/web/buy-manas/src/components/Profile/Description/Favorites/Favorites.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styles from './Favorites.module.css'
+import { NavLink } from 'react-router-dom'
+import { IMAGES_URL } from '../../../../constants'
+import { useTranslation } from 'react-i18next'
+
+const Favorites = (props) => {
+    const { t } = useTranslation();
+    return (
+        <div className={styles.favoritePosts}>
+            {props.favoritePosts.length
+                ? props.favoritePosts.map(p => <NavLink to={`/posts/${p.poster[0].id}`}>
+                    <div className={styles.favoritePostItem} key={p.poster[0].id}>
+                        <div className={styles.imgBlock}>
+                            {p.poster[0].images.length ?
+                                <img src={IMAGES_URL + p.poster[0].images[0].url} alt="categoryIcon" />
+                                :
+                                <img src={require('../../../../assets/images/logo.png')} alt="categoryIcon" />
+                            }
+                        </div>
+                        <div className={styles.descriptionBlock}>
+                            <p className={styles.postTitle}>{p.poster[0].title}</p>
+                            {/* <p className={styles.description}>{p.poster[0].description}</p> */}
+                            <p className={styles.cost}> {p.poster[0].cost != null ? p.poster[0].cost + ' сом' : t('contract')}</p>
+                        </div>
+
+                    </div>
+                </NavLink>)
+                : <p className={styles.nullPosts}>Пока нет постов((</p>
+            }
+            {props.hasMore && props.favoritePosts.length && <div className='loader'>Loading...</div>}
+        </div>
+    )
+}
+
+export default Favorites
---
+++ src/client/web/buy-manas/src/components/Profile/Description/Favorites/FavoritesContainer.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Favorites from './Favorites'
+import { connect } from 'react-redux'
+import { setFavoritePosts, setCurrentPage } from '../../../../actions/posts'
+
+class FavoritesContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasMore: true
+        }
+    }
+    isBottom(el) {
+        return el.getBoundingClientRect().bottom <= window.innerHeight;
+    } 
+
+    componentWillUnmount() {
+        document.removeEventListener('scroll', this.trackScrolling);
+    }
+
+    trackScrolling = () => {
+        const wrappedElement = document.getElementById('root');
+
+        if (this.isBottom(wrappedElement)) {
+            this.props.setCurrentPage(this.props.currentPage + 1);
+            this.setState({ hasMore: false })
+            document.removeEventListener('scroll', this.trackScrolling); 
+        }
+    }; 
+
+    componentDidMount(){
+        this.props.setFavoritePosts(this.props.userId);
+        document.addEventListener('scroll', this.trackScrolling)
+}
+    
+    render() {
+        return (
+            <Favorites {...this.props} hasMore={this.state.hasMore}/>
+        )
+    }
+};
+
+const mapStateToProps = (state) => ({
+    favoritePosts: state.profilePage.favoritePosts,
+    userId: state.auth.id,
+    currentPage: state.postsData.currentPage
+})
+
+
+export default connect(mapStateToProps, { setFavoritePosts, setCurrentPage })(FavoritesContainer)
